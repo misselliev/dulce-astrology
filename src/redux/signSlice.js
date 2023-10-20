7f146182ceb6a05764b3a7234614b291ee8c810b
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-19 17:01:31 -0700

    updating sign slice

--- src/redux/signSlice.js
+++ src/redux/signSlice.js
@@ -1,37 +1,39 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import signData from '../data/signsData.json';
 
-const apiURL = '';
-export const fetchSignInfo = createAsyncThunk('sign/fetchSignInfo', async(sign, thunkAPI) => {
-    try{
-        const response = `${apiURL}_${sign}`
-        return response 
-    }catch(error){
-        return thunkAPI.rejectWithValue({error: error.message})
-    }
-})
-
+export const fetchSignInfo = createAsyncThunk('sign/fetchSignInfo', async (sign, thunkAPI) => {
+  try {
+    const response = await signData.signs[sign];
+    return response;
+  } catch (error) {
+    return thunkAPI.rejectWithValue({ error: error.message });
+  }
+});
 
 const signSlice = createSlice({
-    name: 'sign',
-    initialState: {
-        sign: null,
-        status: 'idle',
-        error: null
-    },
-    reducers : {
-
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchSignInfo.fulfilled, (state, action) => {
-            state.sign = action.payload
-            state.status = 'success'
-        }).addCase(fetchSignInfo.pending, (state) => {
-            state.status = 'loading'
-        }).addCase(fetchSignInfo.rejected, (state, action) => {
-            state.error = action.payload.error
-            state.status = 'rejected'
-        })
-    }
-})
+  name: 'sign',
+  initialState: {
+    sign: null,
+    status: 'idle',
+    error: null,
+    data: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSignInfo.fulfilled, (state, action) => {
+        state.sign = action.payload.name;
+        state.data = action.payload;
+        state.status = 'success';
+      })
+      .addCase(fetchSignInfo.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchSignInfo.rejected, (state, action) => {
+        state.error = action.payload.error;
+        state.status = 'rejected';
+      });
+  },
+});
 
 export default signSlice;
