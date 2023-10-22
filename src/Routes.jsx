cf7868a2e2cfdfc565c23fb79971b517a521c79f
Elizabeth Villalejos <elizabeth.villalejos@gmail.com>
2023-10-22 16:58:41 -0700

    adding routing for horoscopes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignsHome from './pages/SignsHome';
 import SignInfo from './components/SignInfo';
+import HoroscopeHome from './pages/HoroscopeHome';
+import DailyHoroscope from './components/DailyHoroscope';
 
 const Navigation = () => {
   return (
@@ -9,6 +11,8 @@ const Navigation = () => {
       <Route exact path="/" element={<Home />} />
       <Route path="/signs" element={<SignsHome />} />
       <Route path="/signs/*" element={<SignInfo />} />
+      <Route path="/horoscopes" element={<HoroscopeHome />} />
+      <Route path="/horoscopes/*" element={<DailyHoroscope />} />
     </Routes>
   );
 };
